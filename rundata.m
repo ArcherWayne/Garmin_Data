88db45190dbbd3e0d65e92b9bb29bf50d0b7ba63
Archer Wayne <archerwayne1216@gmail.com>
2024-06-11 09:27:42 +0800

    Update rundata.m

--- rundata.m
+++ rundata.m
@@ -30,55 +30,83 @@ AverageSlopeAdjustedPace = Activities(1:end, 21);
 AveragePower = Activities(1:end, 24);
 MaxPower = Activities(1:end, 25);
 
-testpace = ReadPaceData(BestPace);
-
-WeeklyTotalDistance = GetWeekDataTotal(Dates, Distance);
 % 总跑量
-DrawBarPlot(Dates, Distance, '周跑量/km')
-% DrawBarPlot(Dates, AverageHR, '平均心率/bts')
+DistanceWeeklyTotal = GetWeeklyDataTotal(Dates, Distance);
+DrawBarPlot(Dates, DistanceWeeklyTotal, '周跑量/km');
+WeeklyCalories = GetWeeklyDataTotal(Dates,	Calories);
 
 %% 
+
 % 按照星期求解周总量
-function weekdatatotal = GetWeekDataTotal(dates, data)
-	weeks = week(dates);
-	months = month(dates);
-	weekdatatotal = accumarray([months(:) weeks(:)], data(:));
-end
-
-% 绘制按周分布的三维柱状图
-function DrawBarPlot(dates, values, zlabelstring)
-
-	% 获取年份、月份和每月的第几周
+function weeklydatatotal = GetWeeklyDataTotal(dates, data)
 	years = year(dates);
 	months = month(dates);
 	weeks = week(dates);
 
-	% 计算每月的第几周
-	startOfMonth = dates - days(day(dates) - 1); % 每个月的第一天
-	weekInMonth = week(dates) - week(startOfMonth) + 1; % 月内的周数
+	startOfMonth = dates - days(day(dates) - 1);
+	weekInMonth = week(dates) - week(startOfMonth) + 1;
 	weekInMonth(weekInMonth > 5) = 5;
 
-	% 按年份、月份和月内周数分组并求和
 	[uniqueYears, ~, yearIdx] = unique(years);
 	[uniqueMonths, ~, monthIdx] = unique(months);
 
-	% 最大周数设置为5周
 	maxWeeks = 5;
-	weeklySum = zeros(length(uniqueYears), length(uniqueMonths), maxWeeks);
+	weeklydatatotal = zeros(length(uniqueYears), length(uniqueMonths), maxWeeks);
 
-	% 将数据填入三维数组
-	for i = 1:length(values)
+	for i = 1:length(data)
 		y = yearIdx(i);
 		m = monthIdx(i);
 		w = weekInMonth(i);
-		weeklySum(y, m, w) = weeklySum(y, m, w) + values(i);
+		weeklydatatotal(y, m, w) = weeklydatatotal(y, m, w) + data(i);
 	end
+end
 
-	% 使用 bar3 函数绘制三维柱状图
+
+% 按照星期求解周平均
+function weeklydataaverage = GetWeeklyDataAverage(dates, data)
+	years = year(dates);
+	months = month(dates);
+	weeks = week(dates);
+
+	startOfMonth = dates - days(day(dates) - 1);
+	weekInMonth = week(dates) - week(startOfMonth) + 1;
+	weekInMonth(weekInMonth > 5) = 5;
+
+	[uniqueYears, ~, yearIdx] = unique(years);
+	[uniqueMonths, ~, monthIdx] = unique(months);
+
+	maxWeeks = 5;
+	weeklydatatotal = zeros(length(uniqueYears), length(uniqueMonths), maxWeeks);
+	weeelydataaverage = zeros(length(uniqueYears), length(uniqueMonths));
+
+	for i = 1:length(data)
+		y = yearIdx(i);
+		m = monthIdx(i);
+		w = weekInMonth(i);
+		weeklydatatotal(y, m, w) = weeklydatatotal(y, m, w) + data(i);
+	end
+end
+
+
+% 绘制按周分布的三维柱状图
+function DrawBarPlot(dates, weeklyData, zlabelstring)
+	years = year(dates);
+	months = month(dates);
+	weeks = week(dates);
+
+	startOfMonth = dates - days(day(dates) - 1);
+	weekInMonth = week(dates) - week(startOfMonth) + 1;
+	weekInMonth(weekInMonth > 5) = 5;
+
+	[uniqueYears, ~, yearIdx] = unique(years);
+	[uniqueMonths, ~, monthIdx] = unique(months);
+
+	maxWeeks = 5;
+
 	figure;
 	for y = 1:length(uniqueYears)
-		subplot(1, length(uniqueYears), y); % 为每一年创建一个子图
-		bar3(squeeze(weeklySum(y, :, :))'); % 转置数据以适应 bar3 的要求
+		subplot(1, length(uniqueYears), y); 
+		bar3(squeeze(weeklyData(y, :, :))'); 
 		xlabel('月份');
 		ylabel('月内周数');
 		zlabel(zlabelstring);
@@ -86,6 +114,15 @@ function DrawBarPlot(dates, values, zlabelstring)
 		set(gca, 'XTick', 1:length(uniqueMonths), 'XTickLabel', {'1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'});
 		set(gca, 'YTick', 1:maxWeeks);
 		set(gca, 'FontName', 'Microsoft YaHei', 'FontSize', 12, 'FontWeight', 'bold', 'FontAngle', 'italic');
+	end
+end
+
+% 读取配速相关数据
+function PaceString = ConvertPaceDataAsString(data)
+	PaceString = cell(length(data), 1); 
+	for i = 1:length(data)
+		dt = datestr(datetime(data(i), 'ConvertFrom', 'excel'));
+		PaceString{i} = dt(end-7:end-3); 
 	end
 end
 
@@ -101,15 +138,3 @@ function DrawHRandPacePlot(dates, AverageHR, MaxHR, AveragePace, BestPace)
 	ylabel('配速')
 end
 
-% 读取配速数据
-function durationStrArray = ReadPaceData(durationString)
-	% 定义一个函数来将常规数字转换为持续时间格式
-	function durationStr = convertToDuration(num)
-		minutes = floor(num); % 取整数部分作为分钟
-		seconds = round((num - minutes) * 60); % 取小数部分转换为秒
-		durationStr = sprintf('%d''%d''''', minutes, seconds); % 格式化为持续时间字符串
-	end
-
-	% 使用数组对每个持续时间常规数字进行转换
-	durationStrArray = arrayfun(@convertToDuration, durationString, 'UniformOutput', false);
-end
